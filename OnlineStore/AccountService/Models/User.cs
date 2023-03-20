f3da2b392349f3a8f086867fc57e557074760fa1
Ali <Ali@DESKTOP-SHBG8HS>
2023-03-20 07:52:31 +0200

    adding account methods

--- OnlineStore/AccountService/Models/User.cs
+++ OnlineStore/AccountService/Models/User.cs
@@ -3,7 +3,9 @@
     public class User
     {
         public Guid  Id { get; set; }
+        public string Name { get; set; }
         public string EmailAddress { get; set; }
         public string Password { get; set; } 
+        public string Role { get; set; }
     }
 }
--- OnlineStore/AccountService/Program.cs
+++ OnlineStore/AccountService/Program.cs
@@ -1,48 +1,60 @@
 using AccountService.Data;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using AccountService.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AccountDBContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters()
-        {
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            ValidAudience = builder.Configuration["Jwt:Audience"],
-            ValidateActor = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ClockSkew = TimeSpan.Zero,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-        };
-    });
-builder.Services.AddAuthorization();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AccountDBContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 using (var scope = app.Services.CreateScope())
 {
-    var dbContext = scope.ServiceProvider.GetRequiredService<AccountDBContext>();
-    dbContext.Database.Migrate();
+    var db = scope.ServiceProvider.GetRequiredService<AccountDBContext>();
+    db.Database.Migrate();
 }
 
-app.UseAuthentication();
-app.UseAuthorization();
+app.MapPost("/register", async (User user, AccountDBContext db) =>
+{
+    await db.Users.AddAsync(user);
+    await db.SaveChangesAsync();
+    return Results.Created("/login", "User registered !");
+});
+
+app.MapPost("/login", async (UserLogin userLogin, AccountDBContext db) =>
+{
+    User? user = await db.Users.FirstOrDefaultAsync(user => user.EmailAddress.Equals(userLogin.EmailAddress) && user.Password.Equals(userLogin.Password));
+    if (user == null)
+    {
+        return Results.NotFound("Invalid login credentials !");
+    }
+    var key = builder.Configuration["Jwt:Key"];
+    if (key == null)
+    {
+        return Results.StatusCode(500);
+    }
+    var claims = new[]
+    {
+        new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+        new Claim(ClaimTypes.Email, user.EmailAddress),
+        new Claim(ClaimTypes.Name, user.Name),
+        new Claim(ClaimTypes.Role, user.Role)
+    };
+    var jwt = new JwtSecurityToken
+    (
+        issuer: builder.Configuration["Jwt:Issuer"],
+        audience: builder.Configuration["Jwt:Audience"],
+        claims: claims,
+        expires: DateTime.UtcNow.AddMinutes(30),
+        notBefore: DateTime.UtcNow,
+        signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)), SecurityAlgorithms.HmacSha256)
+    );
+    var jwtString = new JwtSecurityTokenHandler().WriteToken(jwt);
+    return Results.Ok(jwtString);
+});
 
 app.Run();
